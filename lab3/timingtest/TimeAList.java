404f3e6e39563c35d3ba112a1c09aeb2faf16239
liuguangyang <liuguangyang1994@gmail.com>
2023-12-27 11:38:10 +0800

    lab3 - timingtest

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -5,6 +5,9 @@ import edu.princeton.cs.algs4.Stopwatch;
  * Created by hug.
  */
 public class TimeAList {
+    public static AList variousTestSize;
+    public static AList testRuntime;
+    public static AList operations;
     private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
@@ -19,9 +22,31 @@ public class TimeAList {
 
     public static void main(String[] args) {
         timeAListConstruction();
+        printTimingTable(variousTestSize, testRuntime, operations);
     }
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> testAlist = new AList<>();
+        variousTestSize = new AList<>();
+        testRuntime = new AList<>();
+        operations = new AList<>();
+        int testSize = 0;
+        while (testAlist.size() < 128000) {
+            if (testSize < 1000) {
+                testSize += 1000;
+            } else {
+                testSize *= 2;
+            }
+            Stopwatch sw = new Stopwatch();
+            while (testAlist.size() < testSize) {
+                testAlist.addLast(0);
+            }
+            double timeInSeconds = sw.elapsedTime();
+
+            variousTestSize.addLast(testAlist.size());
+            testRuntime.addLast(timeInSeconds);
+            operations.addLast(testAlist.size());
+        }
     }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -5,6 +5,10 @@ import edu.princeton.cs.algs4.Stopwatch;
  * Created by hug.
  */
 public class TimeSLList {
+    public static AList<Integer> variousTestSize;
+    public static AList<Double> testRuntime;
+    public static AList<Integer> operations;
+
     private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
         System.out.printf("------------------------------------------------------------\n");
@@ -19,10 +23,39 @@ public class TimeSLList {
 
     public static void main(String[] args) {
         timeGetLast();
+        printTimingTable(variousTestSize, testRuntime, operations);
     }
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
-    }
+        SLList<Integer> testAlist = new SLList<>();
+        variousTestSize = new AList<>();
+        testRuntime = new AList<>();
+        operations = new AList<>();
+        int testSize = 0;
+        while (testAlist.size() < 128000) {
+            if (testSize < 1000) {
+                testSize += 1000;
+            } else {
+                testSize *= 2;
+            }
 
+            while (testAlist.size() < testSize) {
+                testAlist.addLast(0);
+            }
+
+            int M = 0; //use M to count times of getLast() opeartions.
+            Stopwatch sw = new Stopwatch();
+            while (M < 10000) {
+                testAlist.getLast();
+                M += 1;
+            }
+            double timeInSeconds = sw.elapsedTime();
+
+            variousTestSize.addLast(testAlist.size());
+            testRuntime.addLast(timeInSeconds);
+            operations.addLast(M);
+        }
+
+    }
 }
