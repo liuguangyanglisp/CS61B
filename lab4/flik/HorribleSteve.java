041ee7417ae43d33de7a6464237f4abaf77b02d9
liuguangyang <liuguangyang1994@gmail.com>
2024-01-04 16:13:37 +0800

    fix bug of  Flik enterprise’s library

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -1,16 +1,13 @@
 package flik;
 
 public class HorribleSteve {
-    int i;
     public static void main(String [] args) throws Exception {
         Integer i = 0;
         for (Integer j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                if (!i.equals(j)) {
                     throw new Exception(
                             String.format("i:%d not same as j:%d ??", i, j));
                 }
-            }
         }
         System.out.println("i is " + i);
     }
