82ac0cdc01197e476f8e186c00b41afb279a5b87
liuguangyang <liuguangyang1994@gmail.com>
2024-02-13 15:33:36 +0800

    Finish Lab 12: Getting Started on Project 3.

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -15,7 +15,7 @@ public class HexWorld {
     private static final int WIDTH = 100;
     private static final int HEIGHT = 100;
 
-    private static final long SEED = 2873123;
+    private static final long SEED = 287313;
     private static final Random RANDOM = new Random(SEED);
 
     /** Picks a RANDOM tile with a 33% change of being
@@ -23,13 +23,14 @@ public class HexWorld {
      *  chance of being empty space.
      */
     private static TETile randomTile() {
-        int tileNum = RANDOM.nextInt(5);
+        int tileNum = RANDOM.nextInt(6);
         switch (tileNum) {
-            case 0: return Tileset.TREE;
-            case 1: return Tileset.FLOWER;
-            case 2: return Tileset.GRASS;
-            case 3: return Tileset.WALL;
-            case 4: return Tileset.MOUNTAIN;
+            case 0: return Tileset.FLOWER;
+            case 1: return Tileset.MOUNTAIN;
+            case 2: return Tileset.SAND;
+            case 3: return Tileset.WATER;
+            case 4: return Tileset.TREE;
+            case 5: return Tileset.GRASS;
             default: return Tileset.AVATAR;
         }
     }
@@ -41,81 +42,105 @@ public class HexWorld {
             this.x = x;
             this.y = y;
         }
+        public Position shift (int x, int y) {
+            Position p = new Position(this.x + x, this.y + y);
+            return p;
+        }
     }
 
-    public static void addHexagon(TETile[][] aWorld, Position p, int s) {
-        TETile tile = randomTile();
-        int rightmostTile = p.x + s + 2 * (s - 1);
-        if (rightmostTile >= aWorld[0].length) {
+    public static void addHexagon(TETile[][] world, Position p, int s) {
+        if (s < 2) {
             return;
         }
-        drawSingleHexagon (aWorld, tile, p, s - 1, s, (2 * s));
+        addHexagonHelp (world, randomTile(), p, s - 1, s);
     }
 
-    private static void drawSingleHexagon (TETile[][] aWorld, TETile tileType,  Position p, int blank, int fill, int HexHeight) {
-        if ((p.y - (HexHeight - 1)) < 0) {
+    /**
+     * addHexagon help function, draw rows recursively, like in-order traversal.
+     */
+    private static void addHexagonHelp (TETile[][] world, TETile tile,  Position p, int b, int t) {
+        //draw first row of Hexagon.
+        Position firstRow = p.shift(b, 0);
+        drawRow(world, tile, firstRow, t);
+
+        //draw rows betweent first and end row.
+        if (b > 0) {
+            Position next = p.shift(0, -1);
+            addHexagonHelp (world, tile, next, b-1, t+2);
+        }
+
+        //draw end row after all the rows have been drawn.
+        Position endRow = firstRow.shift(0, -(2 * b + 1));
+        drawRow(world, tile, endRow, t);
+    }
+
+    /**
+     *Draw a row in world with tile from Positon P.
+     */
+    private static void drawRow (TETile[][] world, TETile tile, Position p, int length) {
+        if (p.x + length > WIDTH) {
             return;
         }
-
-        drawRow(aWorld, tileType, p, blank, fill);
-        if (blank > 0) {
-            drawSingleHexagon (aWorld, tileType, new Position(p.x, p.y - 1), (blank - 1), (fill + 2), HexHeight - 2);
+        for (int n = 0; n < length; n ++) {
+            world[p.x + n][p.y] = tile;
         }
-        drawRow(aWorld, tileType, new Position(p.x, (p.y - (HexHeight - 1))), blank, fill);
     }
 
-    private static void drawRow (TETile[][] aWorld, TETile tileType, Position p, int blank, int fill) {
-        int worldWidth = aWorld[0].length;
-        int px = p.x;
-        int py = p.y;
-        int maxPx = px + blank + fill - 1;
-        if (px >= worldWidth || maxPx >= worldWidth) {
+    /*
+    *Draw n Hexagons(each one has side length of s) in a column.
+    */
+    public static void addHexagonColumn (TETile[][] world, Position p, int n, int s) {
+        if (n == 0) {
             return;
         }
-
-        while (blank > 0) {
-            px ++;
-            blank --;
-        }
-
-        while (fill > 0) {
-            aWorld[px][py] = tileType;
-            px ++;
-            fill --;
-        }
+        addHexagon(world, p, s);
+        Position next = p.shift(0, -2*s);
+        addHexagonColumn(world, next, n-1, s);
     }
 
-    public static boolean verticalTesselation (TETile[][] aWorld, Position p, int s) {
-        int worldHeight = aWorld.length;
-        if (p.y >= worldHeight || p.y < 0) {
-            return false;
-        }
-        int spaceForNextHexagon = (p.y + 1) / (2 * s);
-        if (spaceForNextHexagon < 1) {
-            return false;
-        }
-        addHexagon(aWorld, p, s);
-        if (spaceForNextHexagon > 1) {
-            verticalTesselation(aWorld, new Position(p.x, p.y - (2 * s)), s);
-        }
-        return true;
-    }
-
-    public static void hexagonTesselation (TETile[][] aWorld, Position p, int s) {
-        int worldWidth = aWorld[0].length;
-        if (p.x >= worldWidth || p.x < 0) {
+    /*HexagonTesselation from middle column(has m hexagons) to side column.
+    *leftmost or rightmost side has n hexagons.
+    * each hexagon has side length s. */
+    public static void hexagonTesselation (TETile[][] world, Position p, int m, int n,  int s) {
+        /**
+         * if hexagonTesselation is beyond limits of world, don't run.
+        int htHeight = n * 2 * s;
+        int htLeftmost = p.x + (m - n) * (-(2 * s) + 1);
+        int htRightmost = p.x + (m - n + 1) * (2 * s) - 1;
+        if (p.y + 1 < htHeight || htLeftmost < 0 || htRightmost > WIDTH) {
             return;
+        }*/
+        addHexagonColumn(world, p, m, s);
+        Position leftColumn = p;
+        Position rightColumn = p;
+        while (m > n) {
+            m --;
+            leftColumn = leftColumn.shift(-(2 * s) + 1, -s);
+            rightColumn = rightColumn.shift((2 * s) - 1, -s);
+            addHexagonColumn(world, leftColumn, m, s);
+            addHexagonColumn(world, rightColumn, m, s);
         }
-        if (verticalTesselation(aWorld, p, s) == true) {
-            int leftPositionX = p.x - (2 * s) + 1;
-            int rightPositionX = p.x + (2 * s) - 1;
-            int newPositionY = p.y - s;
-            Position leftPosition = new Position(leftPositionX, newPositionY);
-            Position rightPosition = new Position(rightPositionX, newPositionY);
-            hexagonTesselation(aWorld, leftPosition, s);
-            hexagonTesselation(aWorld, rightPosition, s);
+
+    }
+
+    /**
+     * Fills the given 2D array of tiles with Nothing.
+     */
+    private static void fillWithNothing(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
         }
     }
+
+    private static void drawWorld(TETile[][] world) {
+        Position p = new Position(50, 99);
+        hexagonTesselation(world, p, 6, 3, 3);
+    }
+
 
 
 
@@ -127,15 +152,8 @@ public class HexWorld {
 
         // initialize tiles
         TETile[][] world = new TETile[WIDTH][HEIGHT];
-        for (int x = 0; x < WIDTH; x += 1) {
-            for (int y = 0; y < HEIGHT; y += 1) {
-                world[x][y] = Tileset.NOTHING;
-            }
-        }
-
-        // fill
-        Position p = new Position(50,99);
-        hexagonTesselation(world, p, 5);
+        fillWithNothing(world);
+        drawWorld(world);
 
         // draws the world to the screen
         ter.renderFrame(world);
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,26 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
-
+Answer: At first, I didn't think about recursion(like in order tranversal) and had no idea.
+Lessons I have learned from video:
+1. Translate difficult and big problem into easy and small subtasks is an important skill and mindset. Draw hexagon in python first, and map it into java.
+2. Do basic small part first, and abstraction on it to do bigger things. To draw hexagon tesselation, draw a hexagon first. To draw a hexagon, draw a row first.
+3. Think in reduction give us more and easier ways to solve a problme. Although this is a java program, it can also solve in python, which means it reduce to python.
+4. Find pattern can make life easier. A hexagon is recursive call to draw rows. A hexagon column is a recursive call to draw hexagon. A hexagon tesselation is recursive call to draw hexagoncolumn. 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: I'm not sure. Maybe rooms and hallways are hexagons. Tessellating hexagons is generating a world?
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: generate room or hallway first, and use abstraction to build bigger world.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: a  room is surrounded by hallway?
